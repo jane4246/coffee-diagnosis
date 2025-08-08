f96749fd50c92c13769681717bc586ddafa76732
maritimjane <58557449+jane4246@users.noreply.github.com>
2025-08-08 10:47:18 -0800

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // You can create this file later
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Coffee Diagnosis App!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
